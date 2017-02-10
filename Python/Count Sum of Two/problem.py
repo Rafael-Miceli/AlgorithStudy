cf97c8be0f0b8c1fc911507fc0040c498fc92906
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-02-10 07:52:26 -0200

    Better code in Count Sum Of Two

--- Python/Count Sum of Two/problem.py
+++ Python/Count Sum of Two/problem.py
@@ -6,17 +6,9 @@ def my_sum(n, l, r):
                 tot += 1
     return tot
 
-def my_sum_2(n, l, r):   
-       
-    result = min((n/2 - l), (r - n/2))    
-
-    if result < 0:
-        return 0    
-
-    if (n & 1):
-        return int(result + 1)
-    else:
-        return int(result + 1)
+def my_sum_2(n, l, r):          
+    result = min((n/2 - l + 1), (r - n/2 + 1))    
+    return 0 if result < 0 else int(result)
 
 def my_sum_3(n, l, r):
     tot = 0
--- Python/Count Sum of Two/test.py
+++ Python/Count Sum of Two/test.py
@@ -4,16 +4,28 @@ from problem import *
 class TestProblem(unittest.TestCase):
 
     #111.804s
-    def test_exemplo_1(self):
-        self.assertEqual(my_sum(50000, 10000, 40000), 15001)
+    # def test_exemplo_1(self):
+    #     self.assertEqual(my_sum(50000, 10000, 40000), 15001)
 
     #0.001s
     def test_exemplo_2(self):
         self.assertEqual(my_sum_2(50000, 10000, 40000), 15001)
 
     #0.012s
-    def test_exemplo_3(self):
-        self.assertEqual(my_sum_3(50000, 10000, 40000), 15001)
+    def test_exemplo_4(self):
+        self.assertEqual(my_sum_3(6, 2, 4), 2)
+
+    def test_exemplo_5(self):
+        self.assertEqual(my_sum_2(6, 3, 3), 1)
+
+    def test_exemplo_6(self):
+        self.assertEqual(my_sum_2(10, 9, 11), 0)
+
+    def test_exemplo_7(self):
+        self.assertEqual(my_sum_2(24, 12, 12), 1)
+
+    def test_exemplo_8(self):
+        self.assertEqual(my_sum_2(93, 24, 58), 12)
 
 
 if __name__ == '__main__':

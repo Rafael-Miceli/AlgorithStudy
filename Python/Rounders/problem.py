6d2e686fa8b0f76e5c1ca40c1ae3ab18f10c1426
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-02-10 13:55:29 -0200

    Melhorando o algo mas ainda o teste nao passa

--- Python/Rounders/problem.py
+++ Python/Rounders/problem.py
@@ -1,17 +1,35 @@
 def rounders(value):            
-    gt_5 = value > 9 and value % 10 >= 5
+
     zeros = 1
-
     while value > 10:            
-
         if value % 10 == 0:
             value = int(value / 10)
             continue
-
+        if value % 10 >= 5:
+            value = int(value / 10)
+            value += 1
+            zeros *= 10
+            continue
+        
         value = int(value / 10)
         zeros *= 10
 
-    value = value + 1 if gt_5 else value
-
     return value * zeros
+
+    
+    # gt_5 = value > 9 and value % 10 >= 5
+    # zeros = 1
+
+    # while value > 10:            
+
+    #     if value % 10 == 0:
+    #         value = int(value / 10)
+    #         continue
+
+    #     value = int(value / 10)
+    #     zeros *= 10
+
+    # value = value + 1 if gt_5 else value
+
+    # return value * zeros
         
